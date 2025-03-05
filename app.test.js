b8c96f369a8a24993bf7f1e3a3fbb1cdecfc0df0
root <root@localhost.localdomain>
2025-03-05 11:20:04 +0530

    Initial commit with Node.js app

---
+++ app.test.js
@@ -0,0 +1,12 @@
+// app.test.js
+const request = require('supertest');
+const app = require('./index'); // import the app
+
+describe('GET /', () => {
+  it('should return Hello, World!', async () => {
+    const response = await request(app).get('/');
+    expect(response.text).toBe('Hello, World!');
+    expect(response.status).toBe(200);
+  });
+});
+
